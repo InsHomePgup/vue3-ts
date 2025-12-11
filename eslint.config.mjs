4e1e97a7a85e5f5f33892548179c1e57254c82f7
GeekAb <47906083+InsHomePgup@users.noreply.github.com>
2025-12-11 11:07:02 +0800

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,10 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  ignores: [
+    'package.json',
+    'pnpm-lock.yaml',
+    'pnpm-workspace.yaml',
+    '.pnpm-debug.log',
+  ],
+})
--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,17 @@
 
 <template>
   <div>
-    Hello World
-    <div>Hello Vue</div>
-    <div>Hello</div>
+    <div class="grid grid-cols-4 w-100">
+      <div v-for="item in 4" :key="item">
+        1
+      </div>
+    </div>
+
+    <div class="grid grid-cols-4 w-100">
+      <div v-for="item in 4" :key="item">
+        12323
+      </div>
+    </div>
   </div>
 </template>
 
